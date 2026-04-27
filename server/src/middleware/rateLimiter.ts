1b31277d22cca5e68efaf2a2ad4f4e66e835c383
maslesa <ljubomirmaslesa@gmail.com>
2026-04-27 20:09:12 +0200

    [fix]: fix rate-limiter keyGenerator function

--- server/src/middleware/rateLimiter.ts
+++ server/src/middleware/rateLimiter.ts
@@ -1,11 +1,11 @@
-import rateLimit from "express-rate-limit";
+import rateLimit, {ipKeyGenerator} from "express-rate-limit";
 
 export const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   message: "Too many requests, try again later.",
-  keyGenerator: (req) => {
-    return (req as any).userId || (req as any).user.userId || req.ip;
+  keyGenerator: (req: any) => {
+    return (req as any).userId || (req as any).user.userId || ipKeyGenerator(req);
   },
 });
 
@@ -13,8 +13,8 @@ export const authLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 5,
   message: "Too many login attempts. Try again later.",
-  keyGenerator: (req) => {
-    return (req as any).userId || (req as any).user.userId || req.ip;
+  keyGenerator: (req: any) => {
+    return (req as any).userId || (req as any).user.userId || ipKeyGenerator(req);
   },
 });
 
@@ -22,15 +22,15 @@ export const uploadTrackLimiter = rateLimit({
   windowMs: 60 * 1000,
   max: 1,
   message: "You can upload only 1 track per minute.",
-  keyGenerator: (req) => {
-    return (req as any).userId || (req as any).user.userId || req.ip;
+  keyGenerator: (req: any) => {
+    return (req as any).userId || (req as any).user.userId || ipKeyGenerator(req);
   },
 });
 
 export const interactionLimiter = rateLimit({
   windowMs: 60 * 1000,
   max: 20,
-  keyGenerator: (req) => {
-    return (req as any).userId || (req as any).user.userId || req.ip;
+  keyGenerator: (req: any) => {
+    return (req as any).userId || (req as any).user.userId || ipKeyGenerator(req);
   },
 });
